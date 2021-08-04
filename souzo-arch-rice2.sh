c05d59eb1e8f9560449b55ebeadf549150fda019
souzo main <49491769+vda789@users.noreply.github.com>
2021-08-04 02:10:04 -0300

    Create souzo-arch-rice2.sh

--- souzo-arch-rice2.sh
+++ souzo-arch-rice2.sh
@@ -7,11 +7,13 @@ git clone git://git.suckless.org/dmenu
 git clone git://git.suckless.org/dwm
 git clone git://git.suckless.org/dwmstatus
 git clone git://git.suckless.org/st
+git clone git://git.suckless.org/slock
 git clone https://aur.archlinux.com/ly
 #instalando
 echo "Instalando pacotes..."
 cd dmenu; make; sudo make install; cd ..;
 cd dwm; make; sudo make install; cd ..;
-cd dwmstatus; sudo make; make install; cd ..;
+cd dwmstatus; make;sudo make install; cd ..;
+cd slock; make; sudo make install; cd ..;
 cd st; make; sudo make install; cd ..;
 cd ly; makepkg -si; cd ..;
