b6474ec03b285b166ee199a9407e746a192fd548
souzo main <49491769+vda789@users.noreply.github.com>
2021-08-04 02:23:34 -0300

    Update souzo-arch-rice2.sh

--- souzo-arch-rice2.sh
+++ souzo-arch-rice2.sh
@@ -3,6 +3,7 @@ cd ~
 echo "Baixando pacotes..."
 mkdir Programs
 cd Programs
+git clone https://aur.archlinux.org/yay.git
 git clone git://git.suckless.org/dmenu
 git clone git://git.suckless.org/dwm
 git clone git://git.suckless.org/dwmstatus
@@ -11,9 +12,25 @@ git clone git://git.suckless.org/slock
 git clone https://aur.archlinux.com/ly
 #instalando
 echo "Instalando pacotes..."
+cd yay; makepkg -si; cd ..;
 cd dmenu; make; sudo make install; cd ..;
 cd dwm; make; sudo make install; cd ..;
 cd dwmstatus; make;sudo make install; cd ..;
 cd slock; make; sudo make install; cd ..;
 cd st; make; sudo make install; cd ..;
 cd ly; makepkg -si; cd ..;
+#brave browser
+yay -S brave-bin
+#telegram
+sudo pacman -S --noconfirm telegram-desktop 
+#vim dots
+echo "Instalando VimDots"
+mkdir /tmp/vimdots
+cd /tmp/vimdots
+git clone https://github.com/Mangeshrex/vim-dots
+rm -rf ~/.vim
+cd vim-dots && mkdir ~/.vim
+cp -r autoload pack session colors ~/.vim
+cp vimrc ~/.vimrc
+#zsh
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
