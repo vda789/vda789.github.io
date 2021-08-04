e41530c61beaff582f7fad5e93edd618668961ac
souzo main <49491769+vda789@users.noreply.github.com>
2021-08-04 02:06:17 -0300

    Update souzo-arch-rice2.sh

--- souzo-arch-rice2.sh
+++ souzo-arch-rice2.sh
@@ -1,12 +1,13 @@
 cd ~
 #baixando
 echo "Baixando pacotes..."
+mkdir Programs
+cd Programs
 git clone git://git.suckless.org/dmenu
 git clone git://git.suckless.org/dwm
 git clone git://git.suckless.org/dwmstatus
 git clone git://git.suckless.org/st
 git clone https://aur.archlinux.com/ly
-mkdir /home/$suser/wallpaper
 #instalando
 echo "Instalando pacotes..."
 cd dmenu; make; sudo make install; cd ..;
@@ -14,5 +15,3 @@ cd dwm; make; sudo make install; cd ..;
 cd dwmstatus; sudo make; make install; cd ..;
 cd st; make; sudo make install; cd ..;
 cd ly; makepkg -si; cd ..;
-#removendo
-rm -rf dmenu;rm -rf dwm;rm -rf dwmstatus;rm -rf st;
